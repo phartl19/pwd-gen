8b911ba96d6980f14aaf9dd9cdafbad00e6da597
phartl19 <Hartl.Paul@sus.htl-grieskirchen.at>
2024-09-18 09:19:36 +0200

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+//generate function to return a password
+
+const generatePassword = () => {
+    const length = 16;
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+    
+    const allCharacters = lowercase + uppercase + numbers + symbols;
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allCharacters.length);
+        password += allCharacters[randomIndex];
+    }
+
+    return password;
+}
+module.exports = generatePassword;
